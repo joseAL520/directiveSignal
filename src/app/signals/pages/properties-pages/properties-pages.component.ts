ce479880fe4ff79a2a4214a0f9ed9bef3b50d11f
jose angel <jose.albelt1817@gmail.com>
2024-06-11 09:37:39 -0500

    properti - mutacion actualiar parte del estado

--- src/app/signals/pages/properties-pages/properties-pages.component.ts
+++ src/app/signals/pages/properties-pages/properties-pages.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interfaces';
 
 @Component({
   selector: 'app-properties-pages',
@@ -7,10 +8,40 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPagesComponent {
 
+  public user = signal<User>({
+    id: 1,
+    email: "george.bluth@reqres.in",
+    first_name: "George",
+    last_name: "Bluth",
+    avatar: "https://reqres.in/img/faces/1-image.jpg"
+  })
 
-  onFieldUpdate(field:string, value:string){
+  onFieldUpdate(field:keyof User, value:string){
 
-    console.log({field, value})
+   this.user.update( curret => {
+
+    switch(field){
+      case 'email':
+        curret.email = value;
+        break
+      case 'avatar':
+        curret.avatar = value;
+        break
+      case 'first_name':
+        curret.first_name = value;
+        break
+
+      case 'last_name':
+        curret.last_name = value;
+        break
+      case 'id':
+        curret.id = Number(value);
+        
+    }
+
+    return curret;
+
+   } )
 
   }
 
