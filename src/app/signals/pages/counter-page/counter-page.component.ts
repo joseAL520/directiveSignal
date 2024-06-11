15fe9cb0c1d0123560f21a11ef21c4b784aa3117
jose angel <jose.albelt1817@gmail.com>
2024-06-11 08:22:58 -0500

    counter signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './counter-page.component.css'
 })
 export class CounterPageComponent {
+
+  public counter = signal(10);
+
+  // Es solo lectura 
+  public squareCounter = computed(() => this.counter() * this.counter());
+
+  increaseBy( value: number){
+    this.counter.update(curret => curret + value );
+  }
 
 }
