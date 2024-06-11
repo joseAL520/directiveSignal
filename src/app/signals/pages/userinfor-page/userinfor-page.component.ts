a917d187e59569a68c4d9f4cbf1b960c095cce56
jose angel <jose.albelt1817@gmail.com>
2024-06-11 08:59:18 -0500

    user inf - signal wth  service

--- src/app/signals/pages/userinfor-page/userinfor-page.component.ts
+++ src/app/signals/pages/userinfor-page/userinfor-page.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interfaces';
 
 @Component({
   selector: 'app-userinfor-page',
   templateUrl: './userinfor-page.component.html',
   styleUrl: './userinfor-page.component.css'
 })
-export class UserinforPageComponent {
+export class UserinforPageComponent  implements OnInit{
+
+
+  private userServices = inject(UsersServiceService);
+  public userId = signal(1);
+  public currentUser = signal<User | undefined>( undefined);
+  public userWasFound = signal(true);
+
+
+
+  ngOnInit(): void {
+    this.loadUser( this.userId() );
+  }
+
+
+  loadUser(id:number){
+
+    if(id<=0)return
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+    
+    this.userServices.getuserById(id)
+      .subscribe(user => {
+        this.currentUser.set(user);
+      })
+  }
+
 
 }
